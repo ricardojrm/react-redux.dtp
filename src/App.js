0bb6fdfb6d5917a5e0c63735c363f20584075c32
ricardo.ramalho <ricardo.ramalho@dataprev.gov.br>
2021-09-01 19:22:34 -0300

    Componente App alterado de função para classe, definindo estado para o componente e rescrevendo o método render, adicionando inclusive o botão para excluir publicações.

--- src/App.js
+++ src/App.js
@@ -1,33 +1,52 @@
 import './App.css';
+import { Component } from 'react';
 
-const posts = [
-  {titulo: "Título da primeira publicação", resumo: "Resumo da primeira publicação.", },
-  {titulo: "Título da segunda publicação", resumo: "Resumo da segunda publicação.", },
-  {titulo: "Título da terceira publicação", resumo: "Resumo da terceira publicação.", },
-  {titulo: "Título da quarta publicação", resumo: "Resumo da quarta publicação.", },
-];
-
-
-function App() 
+class App extends Component
 {
-  const publicarPost = function( post )
+
+  initialState = {
+    posts: [
+      {id: 1, titulo: "Título da primeira publicação", resumo: "Resumo da primeira publicação.", },
+      {id: 2, titulo: "Título da segunda publicação", resumo: "Resumo da segunda publicação.", },
+      {id: 3, titulo: "Título da terceira publicação", resumo: "Resumo da terceira publicação.", },
+      {id: 4, titulo: "Título da quarta publicação", resumo: "Resumo da quarta publicação.", },
+    ]
+  }
+
+  state = this.initialState;
+  
+  publicarPost = ( post ) =>
   {
     return (
-      <div>
+      <div key={post.id} className="post">
         <h5>{post.titulo}</h5>
         <p>{post.resumo}</p>
+        <button onClick={_ => this.removerPost( post.id )}>Excluir</button>
         <hr />
       </div>
     );
   }
+  
+  removerPost = ( id ) =>
+  {
+    console.log( "Solicitado a remoção da publicação com identificador", id );
+    let newPosts = this.state.posts.filter( e => e.id !== id );
+    this.setState( {posts: newPosts} );
+  }
 
-  return (
-    <div className="app">
-      <header className="App-header">
-        {posts.map( publicarPost )}
-      </header>
-    </div>
-  );
+  reset = () =>
+  {
+    this.setState( this.initialState );
+  }
+
+  render()
+  {
+    return (
+      <div className="app">
+            {this.state.posts.map( this.publicarPost )}
+      </div>
+    );
+  }
 }
 
 export default App;
