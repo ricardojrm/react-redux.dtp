d8edebcaa513444a4899522eda7905549fc06fc5
ricardo.ramalho <ricardo.ramalho@dataprev.gov.br>
2021-09-01 10:31:28 -0300

    Criação da estrutura inicial e primeiro uso de jsx

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const posts = [
+  {titulo: "Título da primeira publicação", resumo: "Resumo da primeira publicação.", },
+  {titulo: "Título da segunda publicação", resumo: "Resumo da segunda publicação.", },
+  {titulo: "Título da terceira publicação", resumo: "Resumo da terceira publicação.", },
+  {titulo: "Título da quarta publicação", resumo: "Resumo da quarta publicação.", },
+];
+
+
+function App() 
+{
+  const publicarPost = function( post )
+  {
+    return (
+      <div>
+        <h5>{post.titulo}</h5>
+        <p>{post.resumo}</p>
+        <hr />
+      </div>
+    );
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {posts.map( publicarPost )}
       </header>
     </div>
   );
