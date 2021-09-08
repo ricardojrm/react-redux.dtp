b18592aedbd8feef1aefafb97ca2887eeb3325f1
ricardo.ramalho <ricardo.ramalho@dataprev.gov.br>
2021-09-08 17:03:18 -0300

    Implementado a lógica de sort de posts.

--- src/App.js
+++ src/App.js
@@ -6,16 +6,28 @@ import Post from './components/Post';
 class App extends Component
 {
 
+  SORT_BY = {
+    ASC: { name: "ASC", compare: function( a, b ) { return a.id - b.id; } },
+    DESC: { name: "DESC", compare: function( a, b ) { return b.id - a.id; } },
+  }
+
   initialState = {
     posts: [
       {id: 1, titulo: "Título da primeira publicação", resumo: "Resumo da primeira publicação.", },
       {id: 2, titulo: "Título da segunda publicação", resumo: "Resumo da segunda publicação.", },
       {id: 3, titulo: "Título da terceira publicação", resumo: "Resumo da terceira publicação.", },
       {id: 4, titulo: "Título da quarta publicação", resumo: "Resumo da quarta publicação.", },
-    ]
+    ],
+    sortBy: this.SORT_BY.ASC,
   }
 
   state = this.initialState;
+
+  handleSort = ( sortBy ) =>
+  {
+    console.log( "Solicitando o reordenamento das publicações usando", sortBy.name );
+    this.setState( {sortBy} );
+  }
   
   removerPost = ( id ) =>
   {
@@ -30,12 +42,21 @@ class App extends Component
     this.setState( this.initialState );
   }
 
+  sortPosts = () =>
+  {
+    const sortBy = this.state.sortBy;
+    console.log( "Reordenando as publicações com", sortBy.name );
+    return this.state.posts.slice().sort( sortBy.compare );
+  }
+
   render()
   {
     return (
       <div className="app">
+          <button onClick={() => this.handleSort( this.SORT_BY.ASC )}>{this.SORT_BY.ASC.name}</button>
+          <button onClick={() => this.handleSort( this.SORT_BY.DESC )}>{this.SORT_BY.DESC.name}</button>
           <If conditional={this.state.posts.length > 0}>
-            {this.state.posts.map( p => <Post key={p.id} post={p} onRemove={() => this.removerPost( p.id )} /> )}
+            {this.sortPosts().map( p => <Post key={p.id} post={p} onRemove={() => this.removerPost( p.id )} /> )}
           </If>
           <If conditional={this.state.posts.length < 1}>
             <p>Nenhuma publicação encontrada.</p>
