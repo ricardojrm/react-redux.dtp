34e356f70b82027bfbb00e3c85e0a174031c5298
ricardo.ramalho <ricardo.ramalho@dataprev.gov.br>
2021-09-10 01:42:37 -0300

    Faça a ordenação automática acontecer pela nota de cada post e veja o que acontece quando os scores vão se alterando.

--- src/components/constants.js
+++ src/components/constants.js
@@ -1,4 +1,4 @@
 export const SORT_BY = {
-    ASC: { name: "ASC", compare: function( a, b ) { return a.timestamp - b.timestamp; } },
-    DESC: { name: "DESC", compare: function( a, b ) { return b.timestamp - a.timestamp; } },
+    ASC: { name: "ASC", compare: function( a, b ) { return a.nota - b.nota; } },
+    DESC: { name: "DESC", compare: function( a, b ) { return b.nota - a.nota; } },
 }
