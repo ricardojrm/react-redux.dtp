6c073975a1e9b5aba4feb09abb0a8b39a2fa7396
ricardo.ramalho <ricardo.ramalho@dataprev.gov.br>
2021-09-17 18:35:31 -0300

    Movendo o tratamento de submit do componente Button para o componente Form para maior semântica.

--- src/components/PostForm.jsx
+++ src/components/PostForm.jsx
@@ -11,6 +11,8 @@ function PostForm( {history} )
 
     function handleSubmit( event )
     {
+        event.preventDefault();
+
         console.log( "Salvando a nova publicação." );
         savePost( {titulo, corpo, autor, categoria} )
         .then( data => {
@@ -21,25 +23,27 @@ function PostForm( {history} )
 
     return (
         <div id="nova-resenha">
-            <div>
-                <label htmlFor="titulo">Título</label>:
-            </div>
-            <input id="titulo" type="text" value={titulo} onChange={(e) => setTitulo( e.target.value )} />
-            <div>
-                <label htmlFor="resenha">Resenha</label>:
-            </div>
-            <textarea id="resenha" type="textarea" rows="5" cols="17" value={corpo} onChange={(e) => setCorpo( e.target.value )} />
-            <div>
-                <label htmlFor="autor">Autor</label>:
-            </div>
-            <input id="autor" type="text" value={autor} onChange={(e) => setAutor( e.target.value )} />
-            <div>
-                <label htmlFor="categoria">Categoria</label>:
-            </div>
-            <input id="categoria" type="text" value={categoria} onChange={(e) => setCategoria( e.target.value )} />
-            <div>
-                <button onClick={handleSubmit}>Taca-lhe o pau!</button>
-            </div>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label htmlFor="titulo">Título</label>:
+                </div>
+                <input id="titulo" type="text" value={titulo} onChange={(e) => setTitulo( e.target.value )} />
+                <div>
+                    <label htmlFor="resenha">Resenha</label>:
+                </div>
+                <textarea id="resenha" type="textarea" rows="5" cols="17" value={corpo} onChange={(e) => setCorpo( e.target.value )} />
+                <div>
+                    <label htmlFor="autor">Autor</label>:
+                </div>
+                <input id="autor" type="text" value={autor} onChange={(e) => setAutor( e.target.value )} />
+                <div>
+                    <label htmlFor="categoria">Categoria</label>:
+                </div>
+                <input id="categoria" type="text" value={categoria} onChange={(e) => setCategoria( e.target.value )} />
+                <div>
+                    <button>Taca-lhe o pau!</button>
+                </div>
+            </form>
         </div>
     );
 }
